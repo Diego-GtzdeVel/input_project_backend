895d8ffe7c8f76c32473bf95153e779bd94bca81
Diego Gtz de Vel <gtzdevel@gmail.com>
2025-06-09 11:48:31 -0600

    Feat: created user and project models for db

---
+++ models/project.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const projectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (v) => validator.isURL(v, { require_protocol: true }),
+      message: (props) => `${props.value} no es un enlace válido de imagen.`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
