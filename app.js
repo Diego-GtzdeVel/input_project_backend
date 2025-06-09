f2a68a7a71078c755eba3162210595a6a8f440eb
Diego Gtz de Vel <gtzdevel@gmail.com>
2025-06-09 16:11:35 -0600

    Fix: added packages and fixed typos

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ const { login, createUser } = require('./controllers/users');
 const usersRouter = require('./routes/users');
 const projectsRouter = require('./routes/projects');
 const auth = require('./middlewares/auth');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const app = express();
 
-const { PORT = 3000 } = process.env
+const { PORT = 3000, JwT_SECRET = 'dev-secret' } = process.env;
 
 app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/inputdb');
+
+app.use(requestLogger);
 
 app.post('/signin', login);
 app.post('/signup', createUser);
@@ -24,16 +27,22 @@ app.post('/signup', createUser);
 app.use('/users', auth, usersRouter);
 app.use('/projects', auth, projectsRouter);
 
+app.use((req, res) => {
+  res.status(404).json({ message: 'Resource not found' });
+});
+
+app.use(errorLogger);
+
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
   res.status(statusCode).send({
     message: statusCode === 500
-      ? 'Se ha producido un error en el servidor'
+      ? 'Server error'
       : message,
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`)
-})
+  console.log(`Server runnign in port ${PORT}`);
+});
--- controllers/projects.js
+++ controllers/projects.js
@@ -14,15 +14,32 @@ module.exports.createProject = (req, res, next) => {
     .then((project) => res.status(201).send(project))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        return next(new BadRequestError('Datos invalidos al crea el proyecto'));
+        return next(new BadRequestError('Data for new project not valid'));
       }
       return next(err);
     });
 };
 
-module.exports.deleteProject = (req, res, send) => {
+module.exports.deleteProject = (req, res, next) => {
   const { projectId } = req.params;
 
   Project.findById(projectId)
-    .then((project))
-}
+    .then((project) => {
+      if (!project) {
+        throw new NotFoundError('Project not found');
+      }
+
+      if (project.owner.toString() !== req.user._id) {
+        throw new ForbiddenError('You cannot delete this project');
+      }
+
+      return Project.findByIdAndDelete(projectId)
+        .then (() => res.send({ message: 'Project deleted' }));
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        return next(new BadRequestError('Invalid project ID'));
+      }
+      return next(err);
+    });
+};
